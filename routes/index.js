566e8df1096393c10ac6d2129bcc4ee5d0383ecb
lgignoux <l.gignoux@neolynk.fr>
2020-01-23 14:26:50 +0100

    Add comments

--- routes/index.js
+++ routes/index.js
@@ -38,10 +38,12 @@ router.get('/api/newGame', (req, res) => {
 router.post('/api/player/:color/play/:columnId', (req, res) => {
     const columnId = req.params.columnId
     const color = parseInt(req.params.color)
+    // We can only play in a column if there is room left
     if (Board.isThereRoomLeftInColumn(columnId, gamestate.board)) {
         gamestate.playerClickedOnWrongColumn = false
         gamestate.board = Board.addPieceInColumn(columnId, color, gamestate.board)
         gamestate.lastColumnPlayed = columnId
+        // Check if we have a winner
         if (Algorithm.hasPlayerWon(color, gamestate.board)) {
             if (color === 1)
                 gamestate.redPlayerHasWon = true
@@ -52,6 +54,7 @@ router.post('/api/player/:color/play/:columnId', (req, res) => {
         }
         moveOnToNextPlayer(gamestate)
     } else {
+        // If a player has clicked on a wrong column, a message will be displayed
         gamestate.playerClickedOnWrongColumn = true
     }
     
